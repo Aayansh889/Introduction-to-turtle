bc93a96249e3fdd4ab1ebe9ba22e93f392776363
Aayansh889 <aayansh.saxena@gmail.com>
2025-12-18 16:22:55 +0530

    Add files via upload

---
+++ Activty2.py
@@ -0,0 +1,22 @@
+import turtle
+
+star=turtle.Turtle()
+
+turtle.forward(70)
+turtle.left(120)
+turtle.forward(70)
+turtle.left(120)
+turtle.forward(70)
+
+turtle.penup()
+turtle.right(150)
+turtle.forward(40)
+
+turtle.pendown()
+turtle.right(90)
+turtle.forward(70)
+turtle.right(120)
+turtle.forward(70)
+turtle.right(120)
+turtle.forward(70)
+turtle.done()
